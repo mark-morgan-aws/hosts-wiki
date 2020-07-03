261d1426dcedc252bbb49e3c1a086f684e6aa4d3
Mark Morgan <mark.morgan.aws@gmail.com>
2020-07-03 17:22:06 +0100

    initial commit - VUE CLI - consume local json

---
+++ src/components/hosts.vue
@@ -0,0 +1,40 @@
+// hosts.vue
+
+<template>
+  <div>
+    <h1>Hosts</h1>
+    <button v-on:click="getHostsData">Get Host Data</button>
+    <div v-for="hostData in hostsDataList" :key="hostData.id" class="hosts-data">
+      <div class="host">
+        <div>
+          <span>{{hostData.hostname}}</span>
+        </div>
+        <div>
+          <span class="domain">{{hostData.domain}}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: "Hosts",
+  data() {
+    return {
+      hostsDataList: []
+    };
+  },
+  methods: {
+    getHostsData() {
+      //fetch("hosts.json")
+      //  .then(response => response.json())
+      //  .then(data => (this.hostsDataList = data));
+
+        axios.get("hosts.json").then(response => (this.hostsDataList = response.data));
+    }
+  }
+};
+</script>
